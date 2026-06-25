52f5ccda6535f72a25d6daa9ed316f2bb23323e4
yousef-yasin <yousefyasin694@gmail.com>
2026-06-25 21:52:08 +0300

    add code

--- 01-pytorch-fundamentals/src/tensors.py
+++ 01-pytorch-fundamentals/src/tensors.py
@@ -1,10 +1,13 @@
 import torch as th
 
-x=th.tensor([1,2,3,4,5])
+x = th.tensor([[1, 2, 3, 4, 5],
+              [2,3,4,5,6]])
 
-print(x.sum())
-print(x.shape)
-print(x.dtype)
-print(x.mean)
-print(x.median)
-print(x.min)
+print("Sum:", x.sum())
+print("Shape:", x.shape)
+print("Data Type:", x.dtype)
+
+print("Median:", x.median())
+print("Min:", x.min())
+print("Max:", x.max())
+print("Dimensions:", x.ndim)
