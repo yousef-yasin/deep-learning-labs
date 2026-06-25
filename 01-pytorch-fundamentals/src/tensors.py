70ec5b9d633f6e3663f40ce5895f1d1152d57fd0
yousef-yasin <yousefyasin694@gmail.com>
2026-06-25 22:41:03 +0300

    add for train

--- 01-pytorch-fundamentals/src/tensors.py
+++ 01-pytorch-fundamentals/src/tensors.py
@@ -1,4 +1,4 @@
-import torch as th
+import torch as th 
 
 x = th.tensor([[1, 2, 3, 4, 5],
               [2,3,4,5,6]])
@@ -11,3 +11,34 @@ print("Median:", x.median())
 print("Min:", x.min())
 print("Max:", x.max())
 print("Dimensions:", x.ndim)
+
+#to go to any index
+
+print(x[0])
+print(x[0][1])
+print(x[1,2])
+#use slicing
+print(x[1,:2])
+print(x[0,2:5])
+
+x[0,0]=20
+print(x[0])
+print("\nZeros:")
+print(th.zeros(3,4))
+
+print("\nOnes:")
+print(th.ones(2,5))
+
+print("\nRandom:")
+print(th.rand(2,3))
+
+print("\nRange:")
+print(th.arange(1,11))
+
+print("\nIdentity Matrix:")
+print(th.eye(4))
+y=th.arange(1,13)
+print(y.shape)
+
+z=y.reshape(3,4)
+print(z)
